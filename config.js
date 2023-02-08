893a9242f5347037ab10c7073f91cedc668b91d0
shamilmyran <97163572+shamilmyran@users.noreply.github.com>
2023-02-08 12:26:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ module.exports = {
     VERSION: 'V 1.0.0', // bot version
     SESSION_ID: process.env.SESSION_ID || 'inrl~VOXjsMq73gW5Co/zHEUP53Y9Xslf8x9rA3dt',//enter your ssid to run bot
     MONGO_URL : process.env.MONGO_URI,//mongo url;
-    PASSWORD: process.env.PASSWORD || '', //set passworkd to use 18+ plugins pss = inrl-bot~md
-    REACT : process.env.REACT || 'false', //to react  almsg automaticaly
-    WARNCOUND : process.env.WARNCOUND || "5", // to kick a person by war cound
+    PASSWORD: process.env.PASSWORD || '1234', //set passworkd to use 18+ plugins pss = inrl-bot~md
+    REACT : process.env.REACT || 'true', //to react  almsg automaticaly
+    WARNCOUND : process.env.WARNCOUND || "2", // to kick a person by war cound
     GROUP_CHAT : process.env.GROUP_CHAT || 'false', // to set chat bot on grp
-    FREE_TXT : process.env.FREE_TXT || 'inrlbotmd',//nothing😉
+    FREE_TXT : process.env.FREE_TXT || 'dominic',//nothing😉
     U_STATUS: process.env.U_STATUS || 'false', //to update profilesatus automaticily
     ALIVETXT: process.env.ALIVETXT || 'MYR', //nothing😛
     ALIVE_DATA : process.env.ALIVE_DATA || "https://i.imgur.com/DyLAuEh.jpg,aliveText,alivebButtenText1,aliveTextButten2",// coustamized alive bottun😛
@@ -22,7 +22,7 @@ module.exports = {
     WELCOME_SET : process.env.WELCOME_SET || "true", //now thi scmd is not creted creating
     EXIT_MSG : process.env.EXIT_MSG || "true", //to use kick or left msg
     CALL_BLOCK : process.env.CALL_BLOCK || "false", // to block pm calling numbers expt sudo
-    STATUS_VIEW : process.env.STATUS_VIEW || 'true',//it read your status automaticaly 
+    STATUS_VIEW : process.env.STATUS_VIEW || 'false',//it read your status automaticaly 
     MENSION: {
         MENSION_AUDIO : process.env.MENSION_AUDIO || "https://i.imgur.com/iUOGcyF.mp4,https://i.imgur.com/e2PKT60.mp4,https://i.imgur.com/5NZDe8m.mp4,https://i.imgur.com/iaHKsGW.mp4, https://i.imgur.com/NGWrUkQ.mp4,https://i.imgur.com/GUtD1hr.mp4, https://i.imgur.com/PBuALN3.mp4, https://i.imgur.com/79YoX98.mp4, https://i.imgur.com/HzIz3ls.mp4, https://i.imgur.com/2jzZauR.mp4, https://i.imgur.com/oKVVwIj.mp4, https://i.imgur.com/AzqHkeV.mp4, https://i.imgur.com/pf1BWXN.mp4, https://i.imgur.com/N2N6S7C.mp4, https://i.imgur.com/IrEdvwL.mp4, https://i.imgur.com/4fdJUoj.mp4, https://i.imgur.com/FaOtaF5.mp4, https://i.imgur.com/wsrdnP8.mp4,https://i.imgur.com/imr3bC8.mp4, https://i.imgur.com/Xgzkbsk.mp4, https://i.imgur.com/0WML13D.mp4, https://i.imgur.com/b491tUX.mp4, https://i.imgur.com/GuAnUuf.mp4, https://i.imgur.com/yVwoqYp.mp4, https://i.imgur.com/WgMgTBP.mp4,https://i.imgur.com/LQBlI1y.mp4,https://i.imgur.com/EQn2BvM.mp4, https://i.imgur.com/SIj26aP.mp4,https://i.imgur.com/IWlTmTE.mp4, https://i.imgur.com/c4E7E1k.mp4, https://i.imgur.com/D5PREWe.mp4,https://i.imgur.com/JL6FWpS.mp4, https://i.imgur.com/rvD8PlZ.mp4, https://i.imgur.com/ChUKdPk.mp4, https://i.imgur.com/z9oQd4N.mp4, https://i.imgur.com/z9oQd4N.mp4, https://i.imgur.com/vkdcGHu.mp4, https://i.imgur.com/vkdcGHu.mp4,https://i.imgur.com/K2QlmTc.mp4, https://i.imgur.com/AeGNKo3.mp4, https://i.imgur.com/Qe5khtL.mp4, https://i.imgur.com/lCZl3Af.mp4, https://i.imgur.com/AsEfT76.mp4, https://i.imgur.com/KraBTPf.mp4, https://i.imgur.com/fbvDI1g.mp4,https://i.imgur.com/uazVUOH.mp4, https://i.imgur.com/YN04ot5.mp4, https://i.imgur.com/YN04ot5.mp4",
         MENSION_IMG : process.env.MENSION_IMG || "https://images.wallpaperscraft.com/image/single/ball_planet_shroud_136916_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/ball_glow_hand_190683_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/ball_hands_glow_166373_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/ball_glow_mask_140903_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/ball_glow_stone_169306_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/ball_glow_reflection_165921_4072x2697.jpg,  https://images.wallpaperscraft.com/image/single/ball_glow_silhouette_149491_1920x2304.jpg,  https://images.wallpaperscraft.com/image/single/ball_glow_line_light_28298_1680x1050.jpg,  https://images.wallpaperscraft.com/image/single/ball_circles_glow_168614_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/ball_circles_neon_137700_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/ball_neon_backlight_147061_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/ball_reflection_neon_168932_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/ball_reflection_hand_184546_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/crystal_ball_ball_hand_142442_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/building_water_minimalism_198457_3145x4718.jpg,  https://images.wallpaperscraft.com/image/single/triangle_inverted_black_white_92770_2560x1600.jpg,  https://images.wallpaperscraft.com/image/single/lamp_electricity_minimalism_128251_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/lamp_electricity_spiral_luminescence_120309_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/lamp_spiral_electricity_light_119929_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/skyscraper_building_architecture_sky_bottom_view_118409_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/skyscraper_bottom_view_building_187222_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/skyscraper_architecture_bottom_view_123571_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/skyscraper_architecture_bottom_view_136419_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/cherries_cherry_berry_122190_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/water_hand_sea_horizon_118181_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/water_hand_stones_148518_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/water_stream_flow_169694_1440x2560.jpg,        https://images.wallpaperscraft.com/image/single/water_horizon_dusk_175987_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/water_moon_dusk_191709_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/water_horizon_sky_164418_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/water_sky_horizon_140454_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/water_horizon_sunset_168843_1440x2560.jpg,  https://images.wallpaperscraft.com/image/single/moon_phases_black_175210_3648x5472.jpg,  https://images.wallpaperscraft.com/image/single/moon_black_sky_191125_2318x2864.jpg,  https://images.wallpaperscraft.com/image/single/stains_paint_mixing_198500_2160x3840.jpg,  https://images.wallpaperscraft.com/image/single/stains_paint_mixing_195514_2160x3840.jpg,  https://images.wallpaperscraft.com/image/single/stains_liquid_paint_191590_2160x3840.jpg,  https://images.wallpaperscraft.com/image/single/stains_liquid_paint_147522_2160x3840.jpg,  https://images.wallpaperscraft.com/image/single/stains_liquid_paint_151928_2160x3840.jpg,  https://images.wallpaperscraft.com/image/single/stains_abstraction_texture_157950_2160x3840.jpg,  https://images.wallpaperscraft.com/image/single/stains_paint_liquid_146393_2160x3840.jpg,  https://images.wallpaperscraft.com/image/single/girl_helmet_cyberpunk_194603_2160x3840.jpg,  https://images.wallpaperscraft.com/image/single/girl_alone_autumn_192003_2160x3840.jpg,  https://images.wallpaperscraft.com/image/single/girl_loneliness_alone_183388_2160x3840.jpg",
@@ -31,7 +31,7 @@ module.exports = {
     LANG : (process.env.LANGUAG || 'EN').toLowerCase(), //values are ml, en only
     OWNER : process.env.OWNER || "918593074023",// bot owner
     BRANCH: 'master',//nothing😉
-    PROFILE_STATUS : process.env.PROFILE_STATUS || "💗 ᴀᴜᴛᴏ ᴍᴀᴛɪᴄ ʙɪᴏ ʙy ɪɴʀʟ-ʙᴏᴛ-ᴍᴅ",// profile status updating value
+    PROFILE_STATUS : process.env.PROFILE_STATUS || "💗 ᴀᴜᴛᴏ ᴍᴀᴛɪᴄ ʙɪᴏ ʙy ᴰᴼᴹᴵᴻᴵᴄ-ʙᴏᴛ-ᴍᴅ",// profile status updating value
     HEROKU: {
         HEROKU: process.env.HEROKU === undefined ? false : convertToBool(process.env.HEROKU),
         API_KEY: process.env.HEROKU_API_KEY || '',
@@ -44,15 +44,15 @@ module.exports = {
     IS_PRESENCE : process.env.IS_PRESENCE || "false",//nothing 😛
     CHATBOT : process.env.CHATBOT || 'false',//to chat bot on bith grp and pm
     FOOTER : process.env.FOOTER || "𝐃𝐎𝐌𝐈𝐍𝐈𝐂",//footers of bottun msg
-    AUDIO_DATA : process.env.AUDIO_DATA || "inrl, inrl,https://i.imgur.com/DyLAuEh.jpg",//audio metadata
-    STICKER_DATA: process.env.STICKER_DATA || "inrl, inrl,https://i.imgur.com/DyLAuEh.jpg",//sticker metedata
-    INSTAGRAM : process.env.INSTAGRAM || "https://tinyurl.com/3ex3e48e",//your ig url
-    PACKNAME : process.env.PACKNAME || "ɪɴʀʟ-ʙᴏᴛ",// packname 
-    GIT : process.env.GIT || "https://tinyurl.com/3ex3e48e",//github url
-    WEB : process.env.WEB || "https://tinyurl.com/ycks3s8p",//your website url
-    YT : process.env.YT || "https://tinyurl.com/36r3668n",//your yt url
-    CAPTION : process.env.CAPTION || "_created by inrl-bot_",//captions of img and videos
-    SUDO: process.env.SUDO || "917593919575",// sudo numbers to manage bot it will been add more by,
-    VIDEO : "https://tinyurl.com/3x38ajmn",//turtorial video to watch how to use bot
-    WAGRP : process.env.WAGRP || 'https://tinyurl.com/f5wh55mk',//your wa group url
+    AUDIO_DATA : process.env.AUDIO_DATA || "Dominic, 💗,https://i.imgur.com/DyLAuEh.jpg",//audio metadata
+    STICKER_DATA: process.env.STICKER_DATA || "ᴰᴼᴹᴵᴻᴵᴄ, 🦋,https://i.imgur.com/DyLAuEh.jpg",//sticker metedata
+    INSTAGRAM : process.env.INSTAGRAM || "https://chat.whatsapp.com/GNWB7BgMduoDij7a64qux4",//your ig url
+    PACKNAME : process.env.PACKNAME || "ᴰᴼᴹᴵᴻᴵᴄ-ʙᴏᴛ",// packname 
+    GIT : process.env.GIT || "https://api.whatsapp.com",//github url
+    WEB : process.env.WEB || "",//your website url
+    YT : process.env.YT || "",//your yt url
+    CAPTION : process.env.CAPTION || "_created by Dominic_",//captions of img and videos
+    SUDO: process.env.SUDO || "918593074023",// sudo numbers to manage bot it will been add more by,
+    VIDEO : "",//turtorial video to watch how to use bot
+    WAGRP : process.env.WAGRP || 'https://api.whatsapp.com/send/?phone=918593074023&text&type=phone_number&app_absent=0',//your wa group url
     };
